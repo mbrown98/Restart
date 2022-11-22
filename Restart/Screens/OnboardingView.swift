2df3809056436c8c8e9d189986b0b8827cba7c2f
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-11-22 18:29:06 -0500

    Switch between onboard and home screen based on onboarding status

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // this true value will only be added to the property when the program does not find the ONBOARDING key in app storage
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     var body: some View {
-        Text("Onboard")
+        VStack(spacing: 20) {
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        }
     }
 }
 
