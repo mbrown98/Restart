4e770b60ea19d57bc29be13e6bec26789004fcf6
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-11-22 18:00:55 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Matthew Brown on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
