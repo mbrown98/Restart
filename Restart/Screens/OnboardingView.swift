de7633c1b7981393e1e2eaf66771faac3009f24d
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-11-22 18:45:19 -0500

    Added onboarding center section

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -34,7 +34,20 @@ struct OnboardingView: View {
                         .padding(.horizontal, 10)
                 }
                 // CENTER
-                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                Spacer()
                 // FOOTER
             }
         }
