53af3e6d7667a9bf21278ad1f74b29d0b16130a1
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-11-22 19:10:49 -0500

    Base HomeView layout

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -13,13 +13,41 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            // HEADER
+            
+            Spacer()
+            
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            // CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // FOOTER
+            
+            Spacer()
+            
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                // button component automatically aligns horizontally
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
         
         
